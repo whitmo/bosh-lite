4d456a6bfb4e82141e3a0c8d9842b21dbe5b287b
Andrea Law <alaw@pivotal.io>
2014-10-27 16:18:49 -0400

    Fix database gems
    Signed-off-by: Andrei Dinin <adinin@pivotal.io>

--- ci/ci-install-stemcell-build-dependencies.sh
+++ ci/ci-install-stemcell-build-dependencies.sh
@@ -4,7 +4,8 @@ main() {
   gem install bundler
 
   # prerequisites for running bundle on bosh
-  sudo apt-get install -y postgres-server-dev-9.3
+  sudo apt-get update
+  sudo apt-get install -y postgresql-server-dev-9.3
   sudo apt-get install -y libsqlite3-dev
   sudo apt-get install -y libmysqlclient-dev
 }
