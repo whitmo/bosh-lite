c98555869da620cae9ed9cde44dae9157cc3b428
Andrea Law <alaw@pivotal.io>
2014-10-27 15:57:01 -0400

    Add prerequisites for database gems
    Signed-off-by: Andrei Dinin <adinin@pivotal.io>

--- ci/ci-install-stemcell-build-dependencies.sh
+++ ci/ci-install-stemcell-build-dependencies.sh
@@ -2,6 +2,11 @@
 
 main() {
   gem install bundler
+
+  # prerequisites for running bundle on bosh
+  sudo apt-get install -y postgres-server-dev-9.3
+  sudo apt-get install -y libsqlite3-dev
+  sudo apt-get install -y libmysqlclient-dev
 }
 
 main
