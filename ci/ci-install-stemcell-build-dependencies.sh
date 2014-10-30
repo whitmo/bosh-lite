9a1a538768329d1174d8bfbf8d3bff0b8d3759f5
Andrea Law <alaw@pivotal.io>
2014-10-30 16:25:23 -0400

    Install the Chef DK for Vagrant
    Signed-off-by: Andrei Dinin <adinin@pivotal.io>

--- ci/ci-install-stemcell-build-dependencies.sh
+++ ci/ci-install-stemcell-build-dependencies.sh
@@ -8,6 +8,10 @@ main() {
   sudo apt-get install -y postgresql-server-dev-9.3
   sudo apt-get install -y libsqlite3-dev
   sudo apt-get install -y libmysqlclient-dev
+
+  # prerequisites for vagrant
+  wget https://opscode-omnibus-packages.s3.amazonaws.com/mac_os_x/10.8/x86_64/chefdk-0.3.2-1.dmg
+  dpkg -i chefdk-0.3.2-1.dmg
 }
 
 main
