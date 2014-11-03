250a28bffba30910aa36cefaa40d85805229f122
Andrea Law <alaw@pivotal.io>
2014-11-03 13:21:57 -0500

    Only install ChefDK if it is not already installed
    Signed-off-by: Andrei Dinin <adinin@pivotal.io>

--- ci/ci-install-stemcell-build-dependencies.sh
+++ ci/ci-install-stemcell-build-dependencies.sh
@@ -12,9 +12,14 @@ main() {
   sudo apt-get install -y libmysqlclient-dev
 
   # prerequisites for vagrant
-  wget http://opscode-omnibus-packages.s3.amazonaws.com/ubuntu/12.04/x86_64/chefdk_0.3.2-1_amd64.deb
 
-  dpkg -i chefdk_0.3.2-1_amd64.deb
+  dpkg -s chefdk 2> /dev/null
+
+  if [ $? -ne 0 ]; then
+    wget http://opscode-omnibus-packages.s3.amazonaws.com/ubuntu/12.04/x86_64/chefdk_0.3.2-1_amd64.deb
+
+    dpkg -i chefdk_0.3.2-1_amd64.deb
+  fi
 }
 
 main
