d3fa1333da937a4cc1cd3dcf533ac320f16d22a6
Andrea Law <alaw@pivotal.io>
2014-11-12 15:21:28 -0500

    Refactor ci helper functions
    Signed-off-by: Andrei Dinin <adinin@pivotal.io>

--- ci/ci-stemcell-build-dependencies.sh
+++ ci/ci-stemcell-build-dependencies.sh
@@ -1,48 +1,11 @@
 #!/bin/bash
 
-install_bundle_prerequisites() {
-  sudo apt-get update
-  sudo apt-get install -y postgresql-server-dev-9.3
-  sudo apt-get install -y libsqlite3-dev
-  sudo apt-get install -y libmysqlclient-dev
-}
-
-install_vagrant_prerequisites() {
-  dpkg -s chefdk 2> /dev/null
-
-  if [ $? -ne 0 ]; then
-    wget http://opscode-omnibus-packages.s3.amazonaws.com/ubuntu/12.04/x86_64/chefdk_0.3.2-1_amd64.deb
-
-    dpkg -i chefdk_0.3.2-1_amd64.deb
-  fi
-}
-
-install_vagrant_plugins() {
-  installed_plugins=`vagrant plugin list`
-
-  for plugin in "vagrant-vmware-fusion" "vagrant-aws" "vagrant-berkshelf" "vagrant-omnibus"; do
-    echo $installed_plugins | grep $plugin > /dev/null
-
-    if [ $? = 1 ]; then
-      vagrant plugin install $plugin
-    else
-      echo "$plugin is already installed"
-    fi
-  done
-}
-
-get_bosh_stemcell_key() {
-  new_key_path=$HOME/.ssh/id_rsa_bosh
-  echo "-----BEGIN RSA PRIVATE KEY-----" > $new_key_path
-  echo $ID_RSA_BOSH | sed 's/\s\+/\n/g' >> $new_key_path
-  echo "-----END RSA PRIVATE KEY-----" >> $new_key_path
-}
+source $(dirname $0)/ci_helpers.sh
 
 main() {
   chruby 2.1.2
 
-  install_bundle_prerequisites
-  gem install bundler
+  install_bundle
 
   install_vagrant_prerequisites
   install_vagrant_plugins
---
+++ ci/ci_helpers.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+set_virtualbox_machine_folder() {
+  VBoxManage setproperty machinefolder /var/vcap/data/vbox_machines
+}
+
+install_bundle() {
+  sudo apt-get update
+  sudo apt-get install -y postgresql-server-dev-9.3
+  sudo apt-get install -y libsqlite3-dev
+  sudo apt-get install -y libmysqlclient-dev
+  gem install bundler
+}
+
+install_vagrant_prerequisites() {
+  dpkg -s chefdk 2> /dev/null
+
+  if [ $? -ne 0 ]; then
+    wget http://opscode-omnibus-packages.s3.amazonaws.com/ubuntu/12.04/x86_64/chefdk_0.3.2-1_amd64.deb
+
+    dpkg -i chefdk_0.3.2-1_amd64.deb
+  fi
+}
+
+install_vagrant_plugins() {
+  installed_plugins=`vagrant plugin list`
+
+  for plugin in "vagrant-vmware-fusion" "vagrant-aws" "vagrant-berkshelf" "vagrant-omnibus"; do
+    echo $installed_plugins | grep $plugin > /dev/null
+
+    if [ $? = 1 ]; then
+      vagrant plugin install $plugin
+    else
+      echo "$plugin is already installed"
+    fi
+  done
+}
+
+get_bosh_stemcell_key() {
+  new_key_path=$HOME/.ssh/id_rsa_bosh
+  echo "-----BEGIN RSA PRIVATE KEY-----" > $new_key_path
+  echo $ID_RSA_BOSH | sed 's/\s\+/\n/g' >> $new_key_path
+  echo "-----END RSA PRIVATE KEY-----" >> $new_key_path
+}
