e6cf59325af63655085d7aefdfd66c958baf308b
Andrea Law <alaw@pivotal.io>
2014-11-12 15:21:28 -0500

    Refactor ci helper functions
    Signed-off-by: Andrei Dinin <adinin@pivotal.io>

--- ci/ci-stemcell-bats-dependencies.sh
+++ ci/ci-stemcell-bats-dependencies.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
+source $(dirname $0)/ci_helpers.sh
+
 main() {
   set_virtualbox_machine_folder
-}
-
-set_virtualbox_machine_folder() {
-  VBoxManage setproperty machinefolder /var/vcap/data/vbox_machines
 }
 
 main
