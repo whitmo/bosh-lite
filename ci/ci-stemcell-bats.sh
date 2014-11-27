b98eedc70f5f55ee32e14749f807c1c81ca02025
Andrea Law <alaw@pivotal.io>
2014-11-27 12:57:11 -0500

    Change the bucket for stemcell
    Signed-off-by: Andrei Dinin <adinin@pivotal.io>

--- ci/ci-stemcell-bats.sh
+++ ci/ci-stemcell-bats.sh
@@ -16,7 +16,7 @@ rm -rf output
 #download bosh stemcell this will have to change to bosh-lite's pipeline bucket afterwards
 STEMCELL="bosh-stemcell-${BOSH_LITE_CANDIDATE_BUILD_NUMBER}-warden-boshlite-${STEMCELL_OS_NAME}-go_agent.tgz"
 rm -f bosh-stemcell*
-wget "https://bosh-jenkins-artifacts.s3.amazonaws.com/bosh-stemcell/warden/${STEMCELL}"
+wget "https://bosh-lite-ci-pipeline.s3.amazonaws.com/${BOSH_LITE_CANDIDATE_BUILD_NUMBER}/bosh-stemcell/warden/${STEMCELL}"
 
 export BAT_STEMCELL=$(pwd)/${STEMCELL}
 run_bats_against 192.168.50.4 $STEMCELL_OS_NAME
