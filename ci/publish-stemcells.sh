909ae2d34417c46b7bef6b5e7312dd1c0674a754
Andrea Law <alaw@pivotal.io>
2014-12-01 10:59:48 -0500

    Fix s3cmd install
    Signed-off-by: Andrei Dinin <adinin@pivotal.io>

--- ci/publish-stemcells.sh
+++ ci/publish-stemcells.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
 set -ex
+source $(dirname $0)/ci_helpers.sh
+sudo apt-get update
+install_s3cmd
 
 env | sort
 
 for os in "ubuntu-trusty" "centos"; do
-  s3cmd put --mime-type=application/x-gtar --no-preserve -P ./bosh-stemcell-$BOSH_LITE_CANDIDATE_BUILD_NUMBER-warden-boshlite-$os-go_agent.tgz s3://bosh-jenkins-artifacts/bosh-stemcell/warden/
-  s3cmd put --mime-type=application/x-gtar --no-preserve -P ./bosh-stemcell-$BOSH_LITE_CANDIDATE_BUILD_NUMBER-warden-boshlite-$os-go_agent.tgz s3://bosh-jenkins-artifacts/bosh-stemcell/warden/latest-bosh-stemcell-warden-$os-go_agent.tgz
+  s3cmd put --mime-type=application/x-gtar --no-preserve -P ./bosh-stemcell-$BOSH_LITE_CANDIDATE_BUILD_NUMBER-warden-boshlite-$os-go_agent.tgz s3://bosh-lite-ci-pipeline/
+  s3cmd put --mime-type=application/x-gtar --no-preserve -P ./bosh-stemcell-$BOSH_LITE_CANDIDATE_BUILD_NUMBER-warden-boshlite-$os-go_agent.tgz s3://bosh-lite-ci-pipeline/latest-bosh-stemcell-warden-$os-go_agent.tgz
 done
 
 # trusty is the default ubuntu stemcell
-s3cmd put -P ./bosh-stemcell-$BOSH_LITE_CANDIDATE_BUILD_NUMBER-warden-boshlite-ubuntu-trusty-go_agent.tgz s3://bosh-jenkins-artifacts/bosh-stemcell/warden/latest-bosh-stemcell-warden-ubuntu.tgz
-s3cmd put -P ./bosh-stemcell-$BOSH_LITE_CANDIDATE_BUILD_NUMBER-warden-boshlite-ubuntu-trusty-go_agent.tgz s3://bosh-jenkins-artifacts/bosh-stemcell/warden/latest-bosh-stemcell-warden.tgz
+s3cmd put -P ./bosh-stemcell-$BOSH_LITE_CANDIDATE_BUILD_NUMBER-warden-boshlite-ubuntu-trusty-go_agent.tgz s3://bosh-lite-ci-pipeline/latest-bosh-stemcell-warden-ubuntu.tgz
+s3cmd put -P ./bosh-stemcell-$BOSH_LITE_CANDIDATE_BUILD_NUMBER-warden-boshlite-ubuntu-trusty-go_agent.tgz s3://bosh-lite-ci-pipeline/latest-bosh-stemcell-warden.tgz
